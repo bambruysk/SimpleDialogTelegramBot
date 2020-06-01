80369ec2dbf823290f9a5ae6a4777405c81c23dc
bambruysk <bambruysk@gmail.com>
2020-06-01 14:27:18 +0000

    simple

--- tgbot/tg/bot.py
+++ tgbot/tg/bot.py
@@ -1,8 +1,13 @@
 from telegram.ext import Updater
-
-token = 
-
-updater = Updater(token='TOKEN', use_context=True)
+import os
+token = os.environ.get("TG_BOT_TOKEN", "")
 
 
+updater = Updater(token=token, use_context=True)
+
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
